6aa96bcb42f73a8a0e56345da1790776c26498ae
taeyeon136 <taeyeon136@googlemail.com>
2025-05-29 15:28:20 +0900

    Add root route

--- server.js
+++ server.js
@@ -83,4 +83,7 @@ app.get('/api/data', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+  res.json({ message: 'Candybrain Backend API' });
+});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
